4408ae770d9161a69826222c44a58df17a1b5f88
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-03-27 01:08:39 +0530

    Updated PUT route, still issue updation pending

--- models/Project.js
+++ models/Project.js
@@ -13,32 +13,32 @@ const newProject = new mongoose.model('Project', ProjectSchema);
 
 const findProject = async (name) => {
     const myProject = await newProject.find({ name: name }).catch((e) => { throw new Error(e) });
-    console.log(myProject);
+    // console.log(myProject);
     return myProject;
 }
 
 const createProject = async (name, issue) => {
     const myProject = await newProject.create({ name: name, issues: [ issue ] }).catch((e) => { throw new Error(e) });
     myProject.save();
-    console.log(myProject);
+    // console.log(myProject);
     return myProject;
 }
 
 const getCorrespondingIssues = async (projectName, { _id, issue_title, issue_text, created_by, assigned_to, status_text, open }) => {
     let myIssues;
-    console.log({ _id, issue_title, issue_text, created_by, assigned_to, status_text, open })
+    // console.log({ _id, issue_title, issue_text, created_by, assigned_to, status_text, open })
     try {
         myIssues = await newProject.aggregate(
             [
                 { $match: { name: projectName } },
                 { $unwind: "$issues" },
-                (_id != undefined) ? ({ $match: { "$issues._id": ObjectId(_id) } }) : ({ $match: {} }),
-                (open != undefined) ? ({ $match: { "$issues.open": open } }) : ({ $match: {} }),
-                (issue_title != undefined) ? ({ $match: { "$issues.issue_title": issue_title } }) : ({ $match: {} }),
-                (issue_text != undefined) ? ({ $match: { "$issues.issue_text": issue_text } }) : ({ $match: {} }),
-                (created_by != undefined) ? ({ $match: { "$issues.created_by": created_by } }) : ({ $match: {} }),
-                (assigned_to != undefined) ? ({ $match: { "$issues.assigned_to": assigned_to } }) : ({ $match: {} }),
-                (status_text != undefined) ? ({ $match: { "$issues.status_text": status_text } }) : ({ $match: {} }),
+                (_id != undefined) ? ({ $match: { "issues._id": ObjectId(_id) } }) : ({ $match: {} }),
+                (open != undefined) ? ({ $match: { "issues.open": open } }) : ({ $match: {} }),
+                (issue_title != undefined) ? ({ $match: { "issues.issue_title": issue_title } }) : ({ $match: {} }),
+                (issue_text != undefined) ? ({ $match: { "issues.issue_text": issue_text } }) : ({ $match: {} }),
+                (created_by != undefined) ? ({ $match: { "issues.created_by": created_by } }) : ({ $match: {} }),
+                (assigned_to != undefined) ? ({ $match: { "issues.assigned_to": assigned_to } }) : ({ $match: {} }),
+                (status_text != undefined) ? ({ $match: { "issues.status_text": status_text } }) : ({ $match: {} }),
             ]
         ).exec();
     } catch (err) {
@@ -51,9 +51,14 @@ const getCorrespondingIssues = async (projectName, { _id, issue_title, issue_tex
     } else {
         myIssues = myIssues.map((item) => item.issues);
     }
-    console.log(myIssues)
+    // console.log(myIssues)
     return myIssues;
 }
+
+// const updateIssue = async (projectName,issueParams) => {
+//     let myProject = await newProject.findOne({name:projectName})
+
+// }
 
 const Project = {
     Project: newProject,
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,8 @@ module.exports = function (app) {
   app.route('/api/issues/:project')
 
     .get(async function (req, res) {
+      console.log('\t\tInside GET Request')
+      console.log('\t\t' + req.url)
       let project = req.params.project;
       // console.log(req.query)
       let issueParams = req.query;
@@ -20,6 +22,7 @@ module.exports = function (app) {
     })
 
     .post(async function (req, res) {
+      console.log('\t\tInside POST Request')
       let project = req.params.project;
 
       const {
@@ -37,7 +40,7 @@ module.exports = function (app) {
       }
 
       const newIssue = await createIssue(issue_title, issue_text, created_by, assigned_to, status_text);
-      console.log(newIssue);
+      // console.log(newIssue);
 
       let myProject = await findProject(project);
 
@@ -55,9 +58,86 @@ module.exports = function (app) {
       res.json(newIssue)
     })
 
-    .put(function (req, res) {
-      let project = req.params.project;
+    .put(async function (req, res) {
 
+      console.log('\n\n\nPUT REQUEST')
+      let project = req.params.project;
+      console.log(req.body)
+      let issueParams = req.body;
+
+      if (!issueParams._id) {
+        console.log('missing _id\n\n')
+        res.json({ error: 'missing _id' });
+        return;
+      }
+
+      if (Object.keys(issueParams).length == 1) {
+        // ONLY ID is given, no update parameters
+        console.log('no update field(s) sent\n\n')
+        res.json({ result: 'no update field(s) sent', '_id': issueParams._id })
+        return;
+      }
+
+      Project.findOne({ name: project }, (err, projectData) => {
+        if (err || !projectData) {
+          res.json({ error: 'could not update', '_id': issueParams._id });
+          return;
+        }
+        else {
+          let issuedData = projectDataissues.id(issueParams._id);
+          if (!issuedData) {
+            res.json({ error: 'could not update', '_id': issueParams._id });
+            return;
+          }
+
+          issuedData.issue_title = issueParams.issue_title || issuedData.issue_title;
+          issuedData.issue_text = issueParams.issue_text || issuedData.issue_text;
+          issuedData.created_by = issueParams.created_by || issuedData.created_by;
+          issuedData.assigned_to = issueParams.assigned_to || issuedData.assigned_to;
+          issuedData.status_text = issueParams.status_text || issuedData.status_text;
+          issuedData.open = issueParams.open || issuedData.open;
+          issuedData.updated_on = new Date();
+
+          projectData.save((err, data) => {
+            if (err || !data) {
+              res.json({ error: 'could not update', '_id': issueParams._id });
+              return;
+            }
+            else {
+              res.json({ result: 'successfully updated', '_id': issueParams._id });
+              return;
+            }
+          })
+        }
+      })
+
+      // let myProject = await findProject(project);
+      // let issuedData = myProject[ 0 ].issues.id(issueParams._id)
+      // console.log(myProject);
+      // console.log('\nIssue:', issuedData);
+      // if (myProject.length == 0 || !issuedData) {
+      //   console.log('\t\t\tCouldnt Update');
+      //   res.json({ error: 'could not update', '_id': issueParams._id })
+      //   return;
+      // }
+
+      // issuedData.issue_title = issueParams.issue_title || issuedData.issue_title;
+      // issuedData.issue_text = issueParams.issue_text || issuedData.issue_text;
+      // issuedData.created_by = issueParams.created_by || issuedData.created_by;
+      // issuedData.assigned_to = issueParams.assigned_to || issuedData.assigned_to;
+      // issuedData.status_text = issueParams.status_text || issuedData.status_text;
+      // issuedData.open = issueParams.open || issuedData.open;
+      // issuedData.updated_on = new Date();
+
+      // myProject.save((err) => {
+      //   if (err) {
+      //     res.json({ error: 'could not update', '_id': issueParams._id });
+      //     return;
+      //   }
+      //   else {
+      //     res.json({ result: 'successfully updated', '_id': issueParams._id })
+      //   }
+      // });
     })
 
     .delete(function (req, res) {
